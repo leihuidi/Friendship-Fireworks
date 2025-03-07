501d533a9a8cdb27c589f379271357a7ff17be57
leihuidi <sailei1234@126.com>
2025-03-07 21:37:01 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const celebrateBtn = document.getElementById('celebrateBtn');
+
+    celebrateBtn.addEventListener('click', function() {
+        // 跳转到烟花页面
+        window.location.href = 'fireworks.html';
+    });
+});
